1cb55b255b3171ce65827606f997358e288f2cbe
brucetrindade <brucetrindade2000@gmail.com>
2022-07-12 13:59:13 -0300

    RecyclerView passed to fragment

--- app/src/main/java/com/example/pokedex/view/PokedexHomeFragment.kt
+++ app/src/main/java/com/example/pokedex/view/PokedexHomeFragment.kt
@@ -13,77 +13,33 @@ import com.example.pokedex.R
 import com.example.pokedex.domain.Pokemon
 import com.example.pokedex.viewmodel.PokemonViewModel
 import com.example.pokedex.viewmodel.PokemonViewModelFactory
+import kotlinx.android.synthetic.main.fragment_pokedex_home.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PokedexHomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PokedexHomeFragment : Fragment(R.layout.fragment_pokedex_home) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        val viewModel by lazy {
+            ViewModelProvider(this, PokemonViewModelFactory())
+                .get(PokemonViewModel::class.java)
         }
 
-      //  val recyclerView by lazy { findViewById<RecyclerView>(R.id.pokemon_recycler) }
-//
-//        val viewModel by lazy {
-//            ViewModelProvider(this, PokemonViewModelFactory())
-//                .get(PokemonViewModel::class.java)
-//        }
-//
-//        viewModel.pokemons.observe(this, Observer { listPokemon ->
-//            loadRecyclerView(listPokemon)
-//        })
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokedex_home, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PokedexHomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PokedexHomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.pokemons.observe(viewLifecycleOwner, Observer { listPokemon ->
+            loadRecyclerView(listPokemon)
+        })
     }
 
     private fun loadRecyclerView(pokemons: List<Pokemon>) {
-      //  recyclerView.layoutManager = GridLayoutManager(this,2)
-      //  recyclerView.adapter = PokemonAdapter(pokemons)
+        pokemon_recycler.layoutManager = GridLayoutManager(context,2)
+        pokemon_recycler.adapter = PokemonAdapter(pokemons)
 
     }
 }
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_pokedex_home, container, false)
+//    }
 
