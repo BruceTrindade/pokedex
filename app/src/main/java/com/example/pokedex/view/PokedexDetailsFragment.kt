3d9a107067864210b1eeb88befd06281c385d7bc
brucetrindade <brucetrindade2000@gmail.com>
2022-07-19 10:44:29 -0300

    image pass to details fragment

--- app/src/main/java/com/example/pokedex/view/PokedexDetailsFragment.kt
+++ app/src/main/java/com/example/pokedex/view/PokedexDetailsFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.pokedex.R
+import kotlinx.android.synthetic.main.fragment_pokedex_details.*
 
 class PokedexDetailsFragment : Fragment() {
+
+    private val args: PokedexDetailsFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,15 @@ class PokedexDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pokedex_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView(view)
+    }
+
+    private fun setupView(view: View) {
+        Glide.with(view).load(args.pokemon.imageUrl).into(pokemon_img_details)
     }
 
 }
