01194c8e819675485b56792ab015dbf193c7d6f3
brucetrindade <brucetrindade2000@gmail.com>
2022-09-13 07:15:33 -0300

    crash solved

--- app/src/main/java/com/example/pokedex/view/PokedexHomeFragment.kt
+++ app/src/main/java/com/example/pokedex/view/PokedexHomeFragment.kt
@@ -1,16 +1,13 @@
 package com.example.pokedex.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedex.R
 import com.example.pokedex.domain.Pokemon
 import com.example.pokedex.viewmodel.PokemonViewModel
@@ -19,17 +16,20 @@ import kotlinx.android.synthetic.main.fragment_pokedex_home.*
 
 class PokedexHomeFragment : Fragment(R.layout.fragment_pokedex_home) {
 
-        val viewModel by lazy {
-            ViewModelProvider(this, PokemonViewModelFactory())
-                .get(PokemonViewModel::class.java)
-        }
+    val viewModel by lazy {
+        ViewModelProvider(this, PokemonViewModelFactory())
+            .get(PokemonViewModel::class.java)
+    }
 
-   private lateinit var pokemonAdapter: PokemonAdapter
+    private lateinit var pokemonAdapter: PokemonAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel.pokemons.observe(viewLifecycleOwner, Observer { listPokemon ->
-            loadRecyclerView(listPokemon)
-        })
+        viewModel.pokemons.observe(
+            viewLifecycleOwner,
+            Observer { listPokemon ->
+                loadRecyclerView(listPokemon)
+            }
+        )
     }
 
     private fun loadRecyclerView(pokemons: List<Pokemon>) {
@@ -37,17 +37,7 @@ class PokedexHomeFragment : Fragment(R.layout.fragment_pokedex_home) {
             val actions = PokedexHomeFragmentDirections.actionFirstDestinationToPokedexDetailsFragment(pokemon)
             findNavController().navigate(actions)
         }
-        pokemon_recycler.layoutManager = GridLayoutManager(context,2)
+        pokemon_recycler.layoutManager = GridLayoutManager(context, 2)
         pokemon_recycler.adapter = this.pokemonAdapter
-
     }
 }
-
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_pokedex_home, container, false)
-//    }
-
--- app/src/main/java/com/example/pokedex/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokedex/viewmodel/PokemonViewModel.kt
@@ -5,35 +5,38 @@ import androidx.lifecycle.ViewModel
 import com.example.pokedex.api.PokemonRepository
 import com.example.pokedex.domain.Pokemon
 
-class PokemonViewModel: ViewModel() {
+class PokemonViewModel : ViewModel() {
     var pokemons = MutableLiveData<List<Pokemon>>()
 
     init {
-        Thread(Runnable {
-            loadPokemons()
-        }).start()
+        Thread(
+            Runnable {
+                loadPokemons()
+            }
+        ).start()
     }
 
-    private fun loadPokemons(){
+    private fun loadPokemons() {
         val pokemonsApiResult = PokemonRepository.listPokemons()
 
         pokemonsApiResult?.results?.let {
-            pokemons.postValue(it.map {  pokemon ->
-                val number = pokemon.url.replace("https://pokeapi.co/api/v2/pokemon/", "")
-                    .replace("/", "").toString()
-                val pokemonApiResult = PokemonRepository.getPokemons(number.toInt())
+            pokemons.postValue(
+                it.map { pokemon ->
+                    val number = pokemon.url.replace("https://pokeapi.co/api/v2/pokemon/", "")
+                        .replace("/", "").toString()
+                    val pokemonApiResult = PokemonRepository.getPokemons(number.toInt())
 
-                pokemonApiResult.let {
-                    Pokemon(
-                        pokemonApiResult?.id.toString(),
-                        pokemonApiResult!!.name,
-                        pokemonApiResult.types.map { type ->
-                            type.type
-                        }
-                    )
+                    pokemonApiResult.let {
+                        Pokemon(
+                            pokemonApiResult?.id.toString(),
+                            pokemonApiResult!!.name,
+                            pokemonApiResult.types.map { type ->
+                                type.type
+                            }
+                        )
+                    }
                 }
-            })
+            )
         }
     }
-
 }
