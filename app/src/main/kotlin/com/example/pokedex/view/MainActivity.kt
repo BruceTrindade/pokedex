df6bc98ac89a786a760e4a2b002107df75d1528f
brucetrindade <brucetrindade2000@gmail.com>
2022-10-15 17:44:48 -0300

    bottom nav

--- app/src/main/kotlin/com/example/pokedex/view/MainActivity.kt
+++ app/src/main/kotlin/com/example/pokedex/view/MainActivity.kt
@@ -2,15 +2,32 @@ package com.example.pokedex.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.pokedex.R
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener { }
+        }
+
     }
 }
