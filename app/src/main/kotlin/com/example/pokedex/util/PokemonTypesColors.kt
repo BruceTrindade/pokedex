632f4693f5cee0c623e775eba629584d5cbeb3d6
brucetrindade <brucetrindade2000@gmail.com>
2022-10-28 14:33:57 -0300

    update colors

--- app/src/main/kotlin/com/example/pokedex/util/PokemonTypesColors.kt
+++ app/src/main/kotlin/com/example/pokedex/util/PokemonTypesColors.kt
@@ -5,24 +5,20 @@ import com.example.pokedex.R
 object PokemonTypesColors {
     fun getTypeColor(type: String): Int {
         return when (type) {
-          "fighting" -> R.color.fighting
-          "flying" -> R.color.flying
-          "poison" -> R.color.poison
-          "ground" -> R.color.ground
-           "rock" -> R.color.rock
-           "bug" -> R.color.bug
-           "ghost" -> R.color.ghost
-           "steel" -> R.color.steel
-           "fire" -> R.color.fire
-           "water" -> R.color.water
-           "grass" -> R.color.grass
-           "electric" -> R.color.electric
-           "psychic" -> R.color.psychic
-           "ice" -> R.color.ice
-           "dragon" -> R.color.dragon
-           "fairy" -> R.color.fairy
-           "dark" -> R.color.dark
-           else -> R.color.white
+            "grass", "bug" -> R.color.bug_grass
+            "water", "fighting", "normal" -> R.color.water_fighting_normal
+            "poison", "ghost" -> R.color.poison_ghost
+            "flying" -> R.color.flying
+            "ground", "rock" -> R.color.rock_ground
+            "steel" -> R.color.steel
+            "fire" -> R.color.fire
+            "electric, psychic" -> R.color.electric_psychic
+            "psychic" -> R.color.psychic
+            "ice" -> R.color.ice
+            "dragon" -> R.color.dragon
+            "fairy" -> R.color.fairy
+            "dark" -> R.color.dark
+            else -> R.color.dragon
         }
     }
 }
