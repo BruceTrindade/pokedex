f5fbcb93d0a0abbbd739d5fa771c1acaa3ad3254
brucetrindade <brucetrindade2000@gmail.com>
2022-11-11 16:33:52 -0300

    POKEDEX-023 - Foundation tests done

---
+++ app/src/debug/java/com/example/pokedex/testing/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.example.pokedex.testing
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
